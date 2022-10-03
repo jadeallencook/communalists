19f60d51352eb36d387bb78d081e02af52642269
Jade Allen Cook <j_allencook@apple.com>
2022-10-02 19:42:27 -0700

    Created Firebase account and added app to project

---
+++ src/firebase.config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBBKtH-EEIOp-0HVpOhQDgr_RKCOeIeXt0',
+	authDomain: 'communalists.firebaseapp.com',
+	projectId: 'communalists',
+	storageBucket: 'communalists.appspot.com',
+	messagingSenderId: '895012652302',
+	appId: '1:895012652302:web:1472a663bf95e88ad1c35f',
+	measurementId: 'G-41Q7T9V1ZW',
+};
+
+export default firebaseConfig;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// firebase
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import FirebaseContext from './contexts/firebase.context';
+
+// style
 import './theme.css';
 import './normalize.css';
 
+// pages
+import Home from './pages/Home';
+
 const elem = document.getElementById('root')!;
 const root = ReactDOM.createRoot(elem);
+const app = initializeApp(firebaseConfig);
 
 root.render(
 	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path="/" element={<h1>Communalists</h1>}></Route>
-			</Routes>
-		</Router>
+		<FirebaseContext.Provider value={{ app }}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+				</Routes>
+			</Router>
+		</FirebaseContext.Provider>
 	</React.StrictMode>
 );
