813f4988029dee70c660357b21c08141461f2400
Jade Allen Cook <j_allencook@apple.com>
2022-10-24 21:12:25 -0700

    Updated create account form style

--- src/components/organisms/CreateAccountForm/style.ts
+++ src/components/organisms/CreateAccountForm/style.ts
@@ -1,9 +1,9 @@
 const style = () => ({
-	'input': {
-		maxWidth: '500px'
+	input: {
+		maxWidth: '500px',
 	},
-	'select, input[name="zipcode"]': {
-		maxWidth: '200px'
+	'select, input[name="zipcode"], input[name="state"]': {
+		maxWidth: '200px',
 	},
 });
 
