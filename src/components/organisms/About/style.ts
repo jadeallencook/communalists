683d536bb29e6b63736b2b828ee86b00fcb8fd30
Jade Allen Cook <j_allencook@apple.com>
2022-11-20 03:13:26 -0800

    Added a about section to the home

---
+++ src/components/organisms/About/style.ts
@@ -0,0 +1,19 @@
+const style = () => ({
+	margin: '15px 0px',
+	paddingTop: '50px',
+	paddingBottom: '50px',
+	backgroundColor: 'var(--bs-secondary)',
+	color: 'var(--white)',
+	h1: {
+		marginTop: '0px',
+		fontWeight: 'bold',
+	},
+	p: {
+		margin: '0px',
+	},
+	'button.btn-secondary': {
+		marginLeft: '15px',
+	},
+});
+
+export default style;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/organisms/Header';
 import { Container } from 'react-bootstrap';
+import About from '../components/organisms/About';
 
 const Home = () => (
 	<Container>
 		<Header />
+		<About />
 	</Container>
 );
 
