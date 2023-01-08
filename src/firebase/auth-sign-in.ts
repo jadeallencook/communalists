bafbb0f2c9c3927552ab1332f0a7769268d2abd9
Jade Allen Cook <j_allencook@apple.com>
2023-01-07 17:18:49 -0800

    feat: added firebase auth to project

---
+++ src/firebase/auth-sign-in.ts
@@ -0,0 +1,14 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from './init-app';
+
+const auth = getAuth(app);
+const authSignIn = (email: string, password: string) =>
+	signInWithEmailAndPassword(auth, email, password)
+		.then(({ user }) => {
+			console.log({ user });
+		})
+		.catch(({ code, message }) => {
+			console.log({ code, message });
+		});
+
+export default authSignIn;
