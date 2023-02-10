2d527ddff4e64c4d96a28e38d1d20ae028405ff6
Jade Allen Cook <j_allencook@apple.com>
2023-02-10 18:17:19 -0500

    fix: added loader and correct timestamp to comments

--- src/components/RequestModal/Comments/style.ts
+++ src/components/RequestModal/Comments/style.ts
@@ -2,6 +2,10 @@ const style = () => ({
     marginBottom: '15px',
     width: '100%',
     boxShadow: 'none',
+    '.spinner-border': {
+        display: 'block',
+        margin: '15px auto',
+    },
 });
 
 export default style;
