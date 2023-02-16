e2f62664dcb2e2686154bc771801b90f9a1b0b9a
Jade Allen Cook <j_allencook@apple.com>
2023-02-15 19:45:18 -0800

    feat: removed email email encoding utility

--- src/utils/email_encoding.ts
+++
@@ -1,8 +0,0 @@
-const dot = '__dot__';
-const at = '__at__';
-
-export const encodeEmail = (email: string) =>
-    email.replace('@', at).replace('.', dot);
-
-export const decodeEmail = (email: string) =>
-    email.replace(at, '@').replace(dot, '.');
