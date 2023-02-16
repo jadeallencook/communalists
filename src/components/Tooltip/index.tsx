c9bb749ddf2612cff7f197887b1d18b4bb2200b3
Abhinav Bhaskar <abhinav@asbhaskar.com>
2023-02-16 17:39:28 -0600

    fix: removing commented out code

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,16 +3,6 @@ import InfoSVG from '@assets/info.svg';
 import styled, { StyledComponent } from 'styled-components';
 import style from './style';
 
-const InfoIcon = 
-	<img
-		src={InfoSVG}
-		style={{
-			cursor: 'pointer',
-			animationDelay: '1s',
-		}}
-		className="animate__animated animate__tada"
-	/>
-
 const Tooltip: StyledComponent = styled(({
 	className,
 	position,
@@ -30,11 +20,7 @@ const Tooltip: StyledComponent = styled(({
 				placement={position}
 				overlay={
 					<RBTooltip id={`tooltip-${position}`}>
-						{/* <div
-							className={className}
-						> */}
-							{children}
-						{/* </div> */}
+						{children}
 					</RBTooltip>
 				}
 			>
