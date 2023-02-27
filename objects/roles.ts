0c4eb77c018a134db12766ac7bd5d68ff9174be2
Jade Allen Cook <j_allencook@apple.com>
2023-02-26 22:19:46 -0800

    feat: added tech and social media roles

--- objects/roles.ts
+++ objects/roles.ts
@@ -3,6 +3,8 @@ import { RoleKeyType, RoleType } from '@custom-types/role';
 const roles: { [key in RoleKeyType]: RoleType } = {
     driver: 'Driver',
     coordinator: 'Coordinator',
+    'tech-support': 'Tech Support',
+    'social-media': 'Social Media',
 };
 
 export default roles;
