f992a3285817da28796f1fcd53a406a10d983a17
Jade Allen Cook <j_allencook@apple.com>
2023-02-26 22:19:53 -0800

    feat: added tech and social media roles

--- types/role.ts
+++ types/role.ts
@@ -1,3 +1,11 @@
-export type RoleKeyType = 'driver' | 'coordinator';
+export type RoleKeyType =
+    | 'driver'
+    | 'coordinator'
+    | 'tech-support'
+    | 'social-media';
 
-export type RoleType = 'Driver' | 'Coordinator';
+export type RoleType =
+    | 'Driver'
+    | 'Coordinator'
+    | 'Tech Support'
+    | 'Social Media';
