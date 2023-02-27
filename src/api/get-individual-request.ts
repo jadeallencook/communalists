5ddfec4b23a19ed39ad991ddd0859a089dca0108
Jade Allen Cook <j_allencook@apple.com>
2023-02-26 22:21:41 -0800

    fix: formatted individual request api

--- src/api/get-individual-request.ts
+++ src/api/get-individual-request.ts
@@ -1,23 +1,17 @@
 import RequestAidInterface from '@interfaces/request-aid';
-import {
-    getFirestore,
-    doc,
-    getDoc,
-} from 'firebase/firestore';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import app from './init-app';
 
 const db = getFirestore(app);
 
-const getIndividualRequest = async (
-    uid: string
-)=> {
-    try{     
-        const docRef = doc(db, "requests", uid)
+const getIndividualRequest = async (uid: string) => {
+    try {
+        const docRef = doc(db, 'requests', uid);
         const docSnap = await getDoc(docRef);
         return docSnap.data() as RequestAidInterface;
-    } catch(err){
-        console.log(err)
-        return null
+    } catch (err) {
+        console.log(err);
+        return null;
     }
 };
 
