e171164648c48f7e3807a411b3f6badf0f128761
Jade Allen Cook <j_allencook@apple.com>
2023-02-26 22:19:27 -0800

    fix: deleted unused partner file

--- objects/partners.ts
+++
@@ -1,8 +0,0 @@
-import { PartnerKeyType, PartnerType } from '@custom-types/partners';
-
-const partners: { [key in PartnerKeyType]: PartnerType } = {
-    NONE: 'None',
-    SBMA: 'South Bay Mutual Aid',
-};
-
-export default partners;
