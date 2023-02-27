801c3e3efdd08b7534bc05a79ed45444ae5de9cb
Jade Allen Cook <j_allencook@apple.com>
2023-02-26 22:18:36 -0800

    fix: updated locations to use counties

--- objects/locations.ts
+++ objects/locations.ts
@@ -1,27 +1,11 @@
 import { LocationKeyType, LocationType } from '@custom-types/locations';
 
 const locations: { [key in LocationKeyType]: LocationType } = {
-    'campbell-ca': 'Campbell, CA',
-    'cupertino-ca': 'Cupertino, CA',
-    'east-palo-alto-ca': 'East Palo Alto, CA',
-    'los-gatos-ca': 'Los Gatos, CA',
-    'menlo-park-ca': 'Menlo Park, CA',
-    'milpitas-ca': 'Milpitas, CA',
-    'morgan-hill-ca': 'Morgan Hill, CA',
-    'mountain-view-ca': 'Mountain View, CA',
-    'palo-alto-ca': 'Palo Alto, CA',
-    'redwood-city-ca': 'Redwood City, CA',
-    'san-jose-downtown-ca': 'San Jose Downtown, CA',
-    'san-jose-east-ca': 'San Jose East, CA',
-    'san-jose-north-ca': 'San Jose North, CA',
-    'san-jose-south-ca': 'San Jose South, CA',
-    'san-sose-west-ca': 'San Jose West, CA',
-    'santa-clara-ca': 'Santa Clara, CA',
-    'saratoga-ca': 'Saratoga, CA',
-    'sunnyvale-ca': 'Sunnyvale, CA',
-    'willow-glenn-ca': 'Willow Glenn, CA',
-    'san-mateo-ca': 'San Mateo, CA',
-    'san-francisco-ca': 'San Francisco, CA'
+    'santa-clara-ca': 'Santa Clara County (South Bay)',
+    'san-mateo-ca': 'San Mateo County (Peninsula)',
+    'alameda-ca': 'Alameda County (Easy Bay)',
+    'contra-costa-ca': 'Contra Costa County (Easy Bay)',
+    'san-francisco-ca': 'San Francisco County (San Francisco)',
 };
 
 export default locations;
