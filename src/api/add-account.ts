04baba5c7a231f72125badd0a3261772ef71e87c
Jade Allen Cook <j_allencook@apple.com>
2023-04-24 11:49:05 -0700

    fix: signing up creates a new account in database

--- src/api/add-account.ts
+++ src/api/add-account.ts
@@ -1,10 +1,17 @@
-import { collection, addDoc, getFirestore } from 'firebase/firestore';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import app from './init-app';
 import AccountInterface from '@interfaces/account';
+import { getAuth } from 'firebase/auth';
 
 const db = getFirestore(app);
 
-const addAccount = async (value: AccountInterface): Promise<any> =>
-    await addDoc(collection(db, 'accounts'), value);
+const addAccount = async (value: AccountInterface): Promise<any> => {
+    const {
+        currentUser: { uid },
+    } = getAuth(app);
+    const accountsRef = doc(db, 'accounts', uid);
+    const response = await setDoc(accountsRef, value);
+    return response;
+};
 
 export default addAccount;
