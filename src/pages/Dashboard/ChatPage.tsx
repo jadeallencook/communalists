8090c614531073cd9e916782a2804fda750f7a06
Jade Allen Cook <j_allencook@apple.com>
2023-04-26 09:46:45 -0700

    fix: added margin to the discord embed

--- src/pages/Dashboard/ChatPage.tsx
+++ src/pages/Dashboard/ChatPage.tsx
@@ -6,6 +6,7 @@ const ChatPage = () => (
             width: '100%',
             height: '60vh',
             minHeight: '500px',
+            marginBottom: '15px',
         }}
         server="1100571624761208865"
         channel="1100571625449070654"
