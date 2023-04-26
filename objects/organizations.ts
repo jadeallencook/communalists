c9106850117e2460049b9441f62222efe7c38b26
Jade Allen Cook <j_allencook@apple.com>
2023-04-26 14:34:17 -0700

    fix: removed sjfnd and svdsa organization for the moment

--- objects/organizations.ts
+++ objects/organizations.ts
@@ -6,8 +6,8 @@ import {
 const organizations: { [key in OrganizationKeyType]: OrganizationType } = {
     NONE: 'No Association',
     SBMA: 'South Bay Mutual Aid',
-    SJFNB: 'San Jose Food Not Bombs',
-    SVDSA: 'Silicon Valley Democratic Socialists of America',
+    // SJFNB: 'San Jose Food Not Bombs',
+    // SVDSA: 'Silicon Valley Democratic Socialists of America',
 };
 
 export default organizations;
--- types/organizations.ts
+++ types/organizations.ts
@@ -1,7 +1,3 @@
-export type OrganizationKeyType = 'NONE' | 'SBMA' | 'SJFNB' | 'SVDSA';
+export type OrganizationKeyType = 'NONE' | 'SBMA';
 
-export type OrganizationType =
-    | 'No Association'
-    | 'South Bay Mutual Aid'
-    | 'San Jose Food Not Bombs'
-    | 'Silicon Valley Democratic Socialists of America';
+export type OrganizationType = 'No Association' | 'South Bay Mutual Aid';
