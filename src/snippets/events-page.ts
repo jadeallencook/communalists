30b13420cc08aa6620dd6f0ce745a76764225db6
Albert Liu <albertymliu@gmail.com>
2023-05-21 14:08:10 -0400

    fix: added translations for the events page copy

---
+++ src/snippets/events-page.ts
@@ -0,0 +1,43 @@
+import type { SnippetInterface } from '.';
+
+const events: SnippetInterface = {
+    ['header']: {
+        english: 'Weekly Community Events',
+        spanish: 'Eventos Comunitarios Semanales',
+        vietnamese: 'Sự kiện cộng đồng hàng tuần',
+        chinese: '每周社区活动',
+    },
+    ['description']: {
+        english:
+            'Check out our list of weekly community events where \
+you can find free food, showers, essential goods, \
+medical care, and more. Our events page is regularly \
+updated with new opportunities to support and be a \
+part of the efforts in your area.',
+        spanish:
+            'Consulte nuestra lista de eventos comunitarios \
+semanales donde puede encontrar comida gratis, duchas, artículos \
+esenciales, atención médica y más. Nuestra página de eventos se \
+actualiza regularmente con nuevas oportunidades para apoyar y \
+ser parte de los esfuerzos en su área.',
+        vietnamese:
+            'Kiểm tra danh sách các sự kiện cộng đồng hàng \
+tuần của chúng tôi, nơi bạn có thể tìm thấy thức ăn miễn phí, vòi hoa sen, \
+hàng hóa thiết yếu, dịch vụ chăm sóc y tế, v.v. Trang sự kiện của chúng tôi \
+được cập nhật thường xuyên với các cơ hội mới để hỗ trợ và là một phần của những nỗ lực trong khu vực của bạn.',
+        chinese:
+            '查看我们的每周社区活动列表，您可以在其中找到免费食物、\
+淋浴、必需品、医疗等。 我们的活动页面会定期更新，提供支持和参与您所在地区的活动的新机会。',
+    },
+    ['footer']: {
+        english:
+            'Stay informed and get involved with your community through our events page.',
+        spanish:
+            'Manténgase informado e involúcrese con su comunidad a través de nuestra página de eventos.',
+        vietnamese:
+            'Cập nhật thông tin và tham gia với cộng đồng của bạn thông qua trang sự kiện của chúng tôi.',
+        chinese: '通过我们的活动页面随时了解情况并参与您的社区。',
+    },
+};
+
+export default events;
