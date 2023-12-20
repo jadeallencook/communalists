88b7da6244f454fc0089ab2ff6cd3504914d9d47
Jade Allen Cook <jadeallencook@gmail.com>
2023-12-19 23:48:27 -0800

    chore: added tanstack router

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,49 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '@mantine/core/styles.css';
 import { MantineProvider, createTheme } from '@mantine/core';
+import {
+    Outlet,
+    RouterProvider,
+    Router,
+    Route,
+    RootRoute,
+} from '@tanstack/react-router';
+
+const rootRoute = new RootRoute({
+    component: () => (
+        <Layout>
+            <Outlet />
+        </Layout>
+    ),
+});
+
+const indexRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/',
+    component: () => <h1>Hello World</h1>,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+const router = new Router({ routeTree });
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
 
 const theme = createTheme({});
 
+const Layout = ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+);
+
 const App = () => (
     <MantineProvider theme={theme}>
-        <h1>Hello World</h1>
+        <Layout>
+            <RouterProvider router={router} />
+        </Layout>
     </MantineProvider>
 );
 
