9aa61ea01bb96d7e8eb5d06c7ad1369fe1ab7562
Jade Allen Cook <jadeallencook@gmail.com>
2023-12-22 23:45:02 -0800

    chore: added app shell to layout component

--- src/layout.tsx
+++ src/layout.tsx
@@ -5,9 +5,49 @@
 */
 
 import React from 'react';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return <div>{children}</div>;
+    const [mobileOpened, { toggle: toggleMobile }] = useDisclosure(false);
+    const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Burger
+                        opened={mobileOpened}
+                        onClick={toggleMobile}
+                        hiddenFrom="sm"
+                        size="sm"
+                    />
+                    <Burger
+                        opened={desktopOpened}
+                        onClick={toggleDesktop}
+                        visibleFrom="sm"
+                        size="sm"
+                    />
+                    <b>Communalists</b>
+                </Group>
+            </AppShell.Header>
+            <AppShell.Navbar p="md">
+                {Array(15)
+                    .fill(0)
+                    .map((_, index) => (
+                        <Skeleton key={index} h={28} mt="sm" animate={false} />
+                    ))}
+            </AppShell.Navbar>
+            <AppShell.Main>{children}</AppShell.Main>
+        </AppShell>
+    );
 };
 
 export default Layout;
