45f8357d7c09ad47e80bc05d81a1306a97366a5a
Jade Allen Cook <jadeallencook@gmail.com>
2024-01-01 00:13:07 -0800

    chore: removed tanstack specific code from routes constants

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -10,34 +10,27 @@ import AccessLevel from './access-level';
  * key is the name of the route and the value is the Route object.
  *
  * Each Route object contains the following properties:
- * - path: the path that will be used in the browser
  * - name: this is the name that will be displayed in the sidebar
  * - description: this is the text that will be displayed below the name
  * - access: this is the access level that the user needs to have to access the route
  */
 
 export interface Route {
-    path: string;
     name: string;
     description: string;
     access: AccessLevel;
-    element: React.ReactNode;
 }
 
 const ROUTES: Record<string, Route> = {
     HOME: {
-        path: '/',
         name: 'Home',
         description: '',
         access: AccessLevel.Guest,
-        element: <></>,
     },
     FORMS: {
-        path: '/dashboard/forms/$id',
         name: 'Forms',
         description: '',
         access: AccessLevel.Member,
-        element: <></>,
     },
 };
 
