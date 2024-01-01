755124071daffb218484078b10e09e1b226c569d
Jade Allen Cook <jadeallencook@gmail.com>
2023-12-31 21:26:38 -0800

    chore: added more links and descriptions to navbar

--- src/base/Navbar.tsx
+++ src/base/Navbar.tsx
@@ -3,12 +3,25 @@ import { AppShell, NavLink } from '@mantine/core';
 
 const Navbar = () => (
     <AppShell.Navbar p="md">
-        <NavLink label="Home" description="Discover Our Community Mission" />
+        <NavLink
+            label="San Jose Mutual Aid"
+            description="Manage Forms & Responses"
+            defaultOpened
+        >
+            <NavLink label="San Jose Mutual Aid" />
+            <NavLink label="San Francisco Food Not Bombs" />
+            <NavLink label="Local 801 Tool Library" />
+            <NavLink label="Create New Organization" />
+        </NavLink>
+        <NavLink
+            variant="filled"
+            label="Home"
+            description="Discover Our Community Mission"
+            active
+        />
         <NavLink label="Dashboard" description="Manage Forms & Responses" />
         <NavLink label="Members" description="View Members In Organization" />
         <NavLink label="Settings" description="Adjust Your Personal Settings" />
-        <NavLink label="Login" description="Sign In to Your Account" />
-        <NavLink label="Logout" description="Securely Log Out" />
     </AppShell.Navbar>
 );
 
