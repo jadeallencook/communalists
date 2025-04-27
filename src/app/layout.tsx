cf0541f6ec995e30a1d0c2bde2bf48139634b2a6
Jade Allen Cook <jadeallencook@gmail.com>
2025-04-26 21:36:21 -0700

    Installed Vercel analytics into Next.js project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
