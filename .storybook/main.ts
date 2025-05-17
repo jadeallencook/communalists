0bdc565e8cc79c17bd6b09a0e1f17c4d8a6b626b
Jade Allen Cook <jadeallencook@gmail.com>
2025-05-17 02:53:46 -0700

    fix(storybook): added tailwind and created button

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,18 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/components/**/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
+    "@storybook/addon-styling-webpack"
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  // staticDirs: ["../public"],
+  staticDirs: ["../public"],
 };
 export default config;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "@/contexts/theme";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
