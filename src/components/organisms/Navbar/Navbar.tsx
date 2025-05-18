98a8de44b17eabce9bc85bba9d3a93c967ae4026
Jade Allen Cook <jadeallencook@gmail.com>
2025-05-18 15:40:01 -0700

    feat(Navbar): setup basic navbar component

---
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/atoms/Button";
+import { FC } from "react";
+
+namespace Navbar {
+  export interface Props {}
+}
+
+const Navbar: FC<Navbar.Props> = ({}) => {
+  return (
+    <>
+      {/* Desktop Menu */}
+      <nav className="hidden justify-between lg:flex">
+        <div className="flex items-center gap-6">
+          {/* Logo */}
+          <a className="flex items-center gap-2">
+            <span className="text-lg font-semibold tracking-tighter">
+              ★ Communalists
+            </span>
+          </a>
+        </div>
+        <div className="flex gap-2">
+          <Button asChild variant="outline" size="sm">
+            <a>Login</a>
+          </Button>
+          <Button asChild size="sm">
+            <a>Signup</a>
+          </Button>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      <div className="block lg:hidden">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <a className="flex items-center gap-2">
+            <span className="text-lg font-semibold tracking-tighter">
+              Communalists
+            </span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Navbar };
