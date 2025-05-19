6dbaf2bd680999bd0944b3d49f283ca5c086193f
Jade Allen Cook <jadeallencook@gmail.com>
2025-05-18 20:25:12 -0700

    fix(Button): moved props to namespace

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -4,6 +4,14 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
+namespace Button {
+  export interface Props
+    extends React.ComponentProps<'button'>,
+      VariantProps<typeof buttonVariants> {
+    asChild?: boolean;
+  }
+}
+
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer',
   {
@@ -32,16 +40,13 @@ const buttonVariants = cva(
   },
 );
 
-function Button({
+const Button = ({
   className,
   variant,
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
+}: Button.Props) => {
   const Comp = asChild ? Slot : 'button';
 
   return (
@@ -51,6 +56,6 @@ function Button({
       {...props}
     />
   );
-}
+};
 
 export { Button, buttonVariants };
