14a34c0745ebb7cb55ee2209bb57018a83bfcf2c
Jade Allen Cook <jadeallencook@gmail.com>
2025-05-19 21:49:21 -0700

    feat(home): setup prismic and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div></div>;
+import { type Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { asImageSrc } from '@prismicio/client';
+import { SliceZone } from '@prismicio/react';
+
+import { createClient } from '@/prismicio';
+import { components } from '@/slices';
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle('home').catch(() => notFound());
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle('home').catch(() => notFound());
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [{ url: asImageSrc(page.data.meta_image) ?? '' }],
+    },
+  };
 }
